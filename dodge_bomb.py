151360664e1216c7a5e4137c8ecaaf52aa8b6c6f
c0a2409471 <c0a2409471@edu.teu.ac.jp>
2025-04-22 17:06:38 +0900

    演習２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -44,13 +44,20 @@ def gameover(screen:pg.Surface)->None:
     screen.blit(naki,[WIDTH*(3/10),HEIGHT/2])
     pg.display.update()
     time.sleep(5)
-
+    
 def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    """
+    サイズの異なる爆弾Surfaceを要素としたリストと加速度リスト
+    を返す
+    """
     bb_accs=[a for a in range(1,11)]
+    bb_imgs=[]
     for r in range(1,11):
         bb_img=pg.Surface((20*r,20*r))
         pg.draw.circle(bb_img,(255,0,0),(10*r,10*r),10*r)
-    return bb_img,bb_accs
+        bb_img.set_colorkey((0,0,0))
+        bb_imgs.append(bb_img)
+    return bb_imgs,bb_accs
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -65,7 +72,6 @@ def main():
     bb_rct=bb_img.get_rect()
     bb_rct.centerx=random.randint(0,WIDTH)
     bb_rct.centery=random.randint(0,HEIGHT)
-    bb_img.set_colorkey((0,0,0))
     vx,vy=+5,+5
     clock = pg.time.Clock()
     tmr = 0
@@ -99,16 +105,16 @@ def main():
         if check_bound(kk_rct) != (True,True):
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx,vy)
         bb_imgs, bb_accs = init_bb_imgs()
         avx = vx*bb_accs[min(tmr//500, 9)]
+        avy=  vy*bb_accs[min(tmr//500, 9)]
         bb_img = bb_imgs[min(tmr//500, 9)]
+        bb_rct.move_ip(avx,avy)
         yoko,tate = check_bound(bb_rct)
         if not yoko:
             vx*=-1
         if not tate:
-            vy*=-1
-            
+            vy*=-1  
         screen.blit(bb_img,bb_rct)
         pg.display.update()
         tmr += 1
